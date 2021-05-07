d35dfd3739b4418f53d1c8b2355103ad3cf9ec6d
Samarna <63645732+Samarna@users.noreply.github.com>
2021-05-07 17:53:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-  form1 = new Form();
-}
+var peopleCount = 0;
 function preload(){
   bg = loadImage("background.jpg");
+} 
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  database = firebase.database();
+  form1 = new Form();
+  form1.display();
+  user = new User();
+  user.getPeopleCount();
 }
 function draw() {
   background(255,255,255);  
-  form1.display();
-  image(bg,0,0,windowWidth,windowHeight);
+  console.log(peopleCount);
+  if(peopleCount){
+    form1.display();
+  }
+  //image(bg,0,0,windowWidth,windowHeight);
 }
